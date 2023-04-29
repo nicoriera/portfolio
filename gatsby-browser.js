8fb86055930599e49d0bde61d73821b9f2278c71
nicolas riera <nicolariera@gmail.com>
2023-04-29 16:07:19 +0200

    chore:add-metadata

---
+++ gatsby-browser.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+const MyApp = ({ element }) => (
+  <React.Fragment>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Portfolio - Nicolas RIERA</title>
+      <meta name="description" content="Ma page est dédiée à présenter mes compétences en développement web, mes projets récents et mes réalisations en tant que développeur web. Avec des compétences en développement front-end et back-end, j'ai créé des sites web réactifs et faciles à naviguer en utilisant des technologies modernes telles que Gatsby, React et Node.js. Mon portfolio comprend des projets passionnants qui démontrent mon expertise en matière de développement web. Explorez mon portfolio pour en savoir plus sur mes compétences et mon travail, et n'hésitez pas à me contacter si vous souhaitez en discuter." />
+
+      <meta property="og:title" content="Portfolio - Nicolas RIERA" />
+      <meta property="og:description" content="Ma page est dédiée à présenter mes compétences en développement web, mes projets récents et mes réalisations en tant que développeur web. Avec des compétences en développement front-end et back-end, j'ai créé des sites web réactifs et faciles à naviguer en utilisant des technologies modernes telles que Gatsby, React et Node.js. Mon portfolio comprend des projets passionnants qui démontrent mon expertise en matière de développement web. Explorez mon portfolio pour en savoir plus sur mes compétences et mon travail, et n'hésitez pas à me contacter si vous souhaitez en discuter." />
+      <meta name="keywords" content="gatsby, react, développement web" />
+      <meta property="og:image" content="./content/images/profil.jpg" />
+      <meta property="og:url" content="https://www.nicolasriera.online/" />
+    </Helmet>
+    {element}
+  </React.Fragment>
+)
+
+export const wrapRootElement = ({ element }) => <MyApp element={element} />
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  siteMetadata: {
+    title: "Porfolio de Nicolas RIERA",
+    description: "Ma page est dédiée à présenter mes compétences en développement web, mes projets récents et mes réalisations en tant que développeur web. Avec des compétences en développement front-end et back-end, j'ai créé des sites web réactifs et faciles à naviguer en utilisant des technologies modernes telles que Gatsby, React et Node.js. Mon portfolio comprend des projets passionnants qui démontrent mon expertise en matière de développement web. Explorez mon portfolio pour en savoir plus sur mes compétences et mon travail, et n'hésitez pas à me contacter si vous souhaitez en discuter.",
+  },
   plugins: [
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://www.nicolasriera.online/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "Nicolas RIERA", // Used in manifest.json
